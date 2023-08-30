7d6d9a3741bdf9eaab68ce79caf12da9dfcbc2b0
ShubhRastogi99 <89254142+ShubhRastogi99@users.noreply.github.com>
2023-08-30 23:06:08 +0530

    kdjk

---
+++ maths/primes.cpp
@@ -0,0 +1,33 @@
+//Seive
+int n;
+vector<bool> is_prime(n+1, true);
+is_prime[0] = is_prime[1] = false;
+for (int i = 2; i <= n; i++) {
+    if (is_prime[i] && (long long)i * i <= n) {
+        for (int j = i * i; j <= n; j += i)
+            is_prime[j] = false;
+    }
+}
+
+//Primality test
+bool isPrime(int x) {
+    for (int d = 2; d * d <= x; d++) {
+        if (x % d == 0)
+            return false;
+    }
+    return x >= 2;
+}
+
+//Prime Factorization
+vector<long long> trial_division1(long long n) {
+    vector<long long> factorization;
+    for (long long d = 2; d * d <= n; d++) {
+        while (n % d == 0) {
+            factorization.push_back(d);
+            n /= d;
+        }
+    }
+    if (n > 1)
+        factorization.push_back(n);
+    return factorization;
+}
