38947c97363960d55863cef38ac2737ac2a5074d
ShubhRastogi99 <89254142+ShubhRastogi99@users.noreply.github.com>
2023-09-03 01:12:21 +0530

    hdfh

---
+++ Number Theory/MatrixExponentiation.cpp
@@ -0,0 +1,13 @@
+vector<vector<int>> matrixExponen(vector<vector<int>> &a, int n){
+	if(n == 0){
+		int sz = a.size();
+		vector<vector<int>> ans(sz, vector<int> (sz, 0));
+		for(int i=0; i<sz; i++) ans[i][i] = 1;
+		return ans;
+	}
+	if(n == 1) return a;
+	vector<vector<int>> temp = matrixExponen(a, n/2);
+	vector<vector<int>> ans = multiply(temp, temp);
+	if(n & 1) ans = multiply(ans, a);
+	return ans; 
+}
